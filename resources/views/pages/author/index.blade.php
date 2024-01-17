057fe55d5311537ab91917115b86c2f33d3f23d3
ikromr04 <ikromr04@gmail.com>
2024-01-17 17:44:28 +0500

    Remove br

--- resources/views/pages/author/index.blade.php
+++ resources/views/pages/author/index.blade.php
@@ -24,13 +24,13 @@
       <p class="author-page__info-item">
         @lang('1 Мая 1972')
       </p>
-      <p class="author-page__info-item">
-        Welcome to my personal website, dedicated to important and relevant topics of modern society. On this website, you will find thoughts and reflections in their brief form, accumulated by me over many years, that reveal new facets of various concepts and definitions, as well as my opinions and views on important issues such as the modern individual, life values and ethics, society, enlightenment, and ultimate questions about the world. <br> <br>
-        The main focus of my work is to contribute to the development of personality and self-realization. Understanding the importance of popularizing knowledge and making information accessible, the thoughts are presented in a way that makes their study easy, interesting, and informative. <br> <br>
-        I have been publishing on the internet since 2017. You can also find my work on Twitter and on other thematic platforms on the internet. To help you better understand my ideas and participate in discussions, you will find links to my Twitter posts on many of my thoughts. This will give you the opportunity to see reactions and comments from other readers, as well as share your own views. <br> <br>
-        Enjoy your reading, friends, we hope it brings you new ideas and inspiration. <br> <br>
-        Sincerely, Zafar Mirzo
-      </p>
+      <div class="author-page__info-item" style="gap: 0;">
+        <p>Welcome to my personal website, dedicated to important and relevant topics of modern society. On this website, you will find thoughts and reflections in their brief form, accumulated by me over many years, that reveal new facets of various concepts and definitions, as well as my opinions and views on important issues such as the modern individual, life values and ethics, society, enlightenment, and ultimate questions about the world.</p>
+        <p>The main focus of my work is to contribute to the development of personality and self-realization. Understanding the importance of popularizing knowledge and making information accessible, the thoughts are presented in a way that makes their study easy, interesting, and informative.</p>
+        <p>I have been publishing on the internet since 2017. You can also find my work on Twitter and on other thematic platforms on the internet. To help you better understand my ideas and participate in discussions, you will find links to my Twitter posts on many of my thoughts. This will give you the opportunity to see reactions and comments from other readers, as well as share your own views.</p>
+        <p>Enjoy your reading, friends, we hope it brings you new ideas and inspiration.</p>
+        <p>Sincerely, Zafar Mirzo</p>
+      </div>
       <p class="author-page__info-item">
         @lang('Социальные сети')
         <a
