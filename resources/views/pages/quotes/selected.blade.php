72c6b48534582c3126d53445a9838b32cd31ff31
ikromr04 <ikromr04@gmail.com>
2024-01-25 16:53:21 +0500

    twitter opengraph

--- resources/views/pages/quotes/selected.blade.php
+++ resources/views/pages/quotes/selected.blade.php
@@ -4,16 +4,20 @@
   @php
     //remove tags and slice body
     $share_text = preg_replace('#<[^>]+>#', ' ', $data->quote->quote);
-    $share_text = mb_strlen($share_text) < 170 ? $share_text : mb_substr($share_text, 0, 166) . '...';
+    // $share_text = mb_strlen($share_text) < 170 ? $share_text : mb_substr($share_text, 0, 166) . '...';
   @endphp
   <meta name="description" content="{{ $share_text }}">
-  <meta property="og:description" content="{{ $share_text }}">
   <meta property="og:title" content="Zafar Mirzo's Author Site" />
+  <meta property="og:type" content="aside" />
+  <meta property="og:url" content="https://zmquotes.com/" />
+  <meta property="og:description" content="{{ $share_text }}">
   <meta property="og:image:alt" content="Zafar Mirzo's Author Site">
   <meta property="og:image" content="{{ asset('favicons/og.png') }}">
-  <meta name="twitter:title" content="Zafar Mirzo's Author Site">
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:image" content="{{ asset('favicons/og.png') }}">
+
+  <meta property="twitter:title" content="Zafar Mirzo's Author Site">
+  <meta property="twitter:card" content="summary" />
+  <meta property="twitter:description" content="{{ $share_text }}">
+  <meta property="twitter:image" content="{{ asset('favicons/og.png') }}">
 @endsection
 
 @section('content')
