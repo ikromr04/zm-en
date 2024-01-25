8abbb94adfa26b8d5764f9f5e1a8321768b3bd63
ikromr04 <ikromr04@gmail.com>
2024-01-25 18:05:54 +0500

    twitter opengraph

--- resources/views/pages/quotes/selected.blade.php
+++ resources/views/pages/quotes/selected.blade.php
@@ -6,10 +6,9 @@
     $share_text = mb_strlen($share_text) < 170 ? $share_text : mb_substr($share_text, 0, 166) . '...';
   @endphp
   <meta name="description" content="{{ $share_text }}">
+  <meta property="og:type" content="article" />
   <meta property="og:title" content="Zafar Mirzo's Author Site" />
-  <meta property="og:type" content="aside" />
   <meta property="og:description" content="{{ $share_text }}">
-  <meta property="og:image:alt" content="Zafar Mirzo's Author Site">
   <meta property="og:image" content="{{ asset('favicons/og.png') }}">
 
   <meta name="twitter:title" content="Zafar Mirzo's Author Site">
